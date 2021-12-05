ad82215aa4c3a0347d48cfdd1d4c3fac1300d550
Carlos J. Rosales Gallegos <carlos@beta-robots.com>
2021-12-05 12:45:24 +0100

    Merge changes by Simon in App and folder structure

--- App.tsx
+++ App.tsx
@@ -3,8 +3,9 @@ import * as React from "react";
 import { Text, View, StyleSheet } from "react-native";
 
 /** expo */
-import Constants from 'expo-constants';
-import * as Device from 'expo-device';
+import Constants from "expo-constants";
+import * as Device from "expo-device";
+import * as Contacts from "expo-contacts";
 
 /** react-navigation */
 import { NavigationContainer } from "@react-navigation/native";
@@ -18,59 +19,59 @@ import { Foundation } from "@expo/vector-icons";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { FontAwesome } from "@expo/vector-icons";
 
-import I18n from "./i18n";
+/** screens/components */
+import News from "./screens/Noticias";
+import Salidas from "./screens/Salidas";
+import Refugios from "./screens/Refugios";
+import Perfil from "./screens/Perfil";
 
-/** screens */
-import News from './screens/Noticias';
-import Salidas from './screens/Salidas';
-import Refugios from './screens/Refugios';
-import Perfil from './screens/Perfil';
+import I18n from "./i18n";
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Tab.Navigator  initialRouteName="Profile"
-          barStyle={{ paddingBottom: 0 }}
-          screenOptions={({ route }) => ({
-            headerStyle: {
-              height: 0,
-            },
-            tabBarIcon: ({ focused, color, size }) => {
-              let iconName;
+	return (
+		<NavigationContainer>
+			<Tab.Navigator
+				initialRouteName="Profile"
+				barStyle={{ paddingBottom: 0 }}
+				screenOptions={({ route }) => ({
+					headerStyle: {
+						height: 0,
+					},
+					tabBarIcon: ({ focused, color, size }) => {
+						let iconName;
 
-              if (route.name === 'News') {
-                iconName = focused
-                  ? 'ios-information-circle'
-                  : 'ios-information-circle-outline';
-                  return <Ionicons name={iconName} size={24} color={color} />;
-              } else if (route.name === 'Trips') {
-                iconName = focused
-                ? 'guide-dog'
-                : 'guide-dog';
-                return <Foundation name={iconName} size={24} color={color} />;
-              } else if (route.name === 'Shelters') {
-                iconName = focused
-                ? 'shield-home'
-                : 'shield-home-outline';
-                return <MaterialCommunityIcons name={iconName} size={24} color={color} />;
-              } else if (route.name === 'Profile') {
-                iconName = focused
-                ? 'id-card'
-                : 'id-card-o';
-                return <FontAwesome name={iconName} size={24} color={color} />
-              }
-            },
-            tabBarActiveTintColor: 'tomato',
-            tabBarInactiveTintColor: 'gray',
-          })
-        }>
-        <Tab.Screen name="News" component={News} options={{ title: '', tabBarLabel: 'Noticias', tabBarBadge: 1,  }}/>
-        <Tab.Screen name="Trips" component={Salidas} options={{ title: '', tabBarLabel: 'Salidas' }}/>
-        <Tab.Screen name="Shelters" component={Refugios}  options={{ title: '', tabBarLabel: 'Refugios' }}/>
-        <Tab.Screen name="Profile" component={Perfil} options={{ title: '', tabBarLabel: 'Perfil' }}/>
-      </Tab.Navigator>
-    </NavigationContainer>
-  );
+						if (route.name === "News") {
+							iconName = focused ? "ios-information-circle" : "ios-information-circle-outline";
+							return <Ionicons name={iconName} size={36} color={color} />;
+						} else if (route.name === "Trips") {
+							iconName = focused ? "guide-dog" : "guide-dog";
+							return <Foundation name={iconName} size={36} color={color} />;
+						} else if (route.name === "Shelters") {
+							iconName = focused ? "shield-home" : "shield-home-outline";
+							return <MaterialCommunityIcons name={iconName} size={36} color={color} />;
+						} else if (route.name === "Profile") {
+							iconName = focused ? "id-card" : "id-card-o";
+							return <FontAwesome name={iconName} size={36} color={color} />;
+						}
+					},
+					tabBarActiveTintColor: "tomato",
+					tabBarInactiveTintColor: "gray",
+				})}>
+				<Tab.Screen
+					name="News"
+					component={News}
+					options={{ title: "", tabBarLabel: I18n.t("noticias"), tabBarBadge: 1 }}
+				/>
+				<Tab.Screen name="Trips" component={Salidas} options={{ title: "", tabBarLabel: I18n.t("salidas") }} />
+				<Tab.Screen
+					name="Shelters"
+					component={Refugios}
+					options={{ title: "", tabBarLabel: I18n.t("refugios") }}
+				/>
+				<Tab.Screen name="Profile" component={Perfil} options={{ title: "", tabBarLabel: I18n.t("perfil") }} />
+			</Tab.Navigator>
+		</NavigationContainer>
+	);
 }
