79a26a806a66309e0db1dd814440df503dd09db9
Carlos J. Rosales Gallegos <carlos@beta-robots.com>
2022-03-23 22:14:25 +0000

    Use ScrollView within View to avoid test fail?

--- screens/Refugios.tsx
+++ screens/Refugios.tsx
@@ -94,7 +94,8 @@ export default function Refugios()
 	const renderItem = ({ item }) => <Refugio refugio={item} />;
 	const insets = useSafeAreaInsets();
 	return (
-		<ScrollView style={[{marginTop: insets.top}, styles.scrollView]} stickyHeaderIndices={[0]}>
+		<View style={{marginTop: insets.top}}>
+		<ScrollView style={styles.scrollView} stickyHeaderIndices={[0]}>
 			{/* sticky header */}
 			<View style={[{ backgroundColor: "white" }, { alignItems: "center" }]}>
 				<Image style={styles.logo} source={require("../assets/logo.jpg")} />
@@ -124,6 +125,7 @@ export default function Refugios()
 				</Text>
 			</View>
 		</ScrollView>
+		</View>
 	);
 }
 
